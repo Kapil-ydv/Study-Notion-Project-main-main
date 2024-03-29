bb11bf086c6cf8a42bdfc618b17740fa18eb8d2f
Kapil-ydv <nahariak264@gmail.com>
2024-03-30 02:33:15 +0530

    updated

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 exports.connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/e_courses")
+    .connect("mongodb://127.0.0.1:27017/e_courses")
     .then(() => console.log("DB Connected Successfully"))
     .catch((error) => {
       console.log("DB Connection Failed");
--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -4,6 +4,6 @@ require("dotenv").config();
 exports.instance = new Razorpay({
   key_id:"rzp_test_2l1U2EWy7WzRyk",
   key_secret:"82ZofjM7ecK9REfwwjtNinc3",
-  // key_id: "rzp_test_Qq1R28RDozLWGC",
-  // key_secret: "97Dbt9GGaIAtnN2CvWDi2Q3s",
+  // key_id: "rzp_test_2l1U2EWy7WzRyk",
+  // key_secret: "82ZofjM7ecK9REfwwjtNinc3",
 });
